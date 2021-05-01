3b621f33f7f083453c574f7e4f0ca9fed96ae748
SugaryLump <sugarcanegaming6000@gmail.com>
2021-05-01 17:16:48 +0100

    Altered hierarchy of goalkeeper

--- src/Classes/Goalkeeper.java
+++ src/Classes/Goalkeeper.java
@@ -1,7 +1,12 @@
 package Classes;
 
-public class Goalkeeper extends Player {
+public class Goalkeeper extends FootballPlayer {
     public int elasticity; //Custom abillity for goalkeepers
+
+    public Goalkeeper(String name, int age, int[] history, int[] ability, int globalAbility, String position, int elasticity) {
+        super(name, age, history, ability, globalAbility, position);
+        this.elasticity = elasticity;
+    }
 
     public int getElasticity() {
         return elasticity;
